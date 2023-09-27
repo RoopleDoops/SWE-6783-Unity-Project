9f5042b0faab6acc7b4225034aa0e6343deac7d8
RoopleDoops <ryanmatthewsmith13@gmail.com>
2023-09-27 18:51:46 -0400

    Enemy Art + Background Implementation + Camera Offset
    -- Added ghost and skeleton art to enemies. Enemies flip sprite based on direction of movement.
    -- Added background canvas to scene. May want to look at better implementation in the future.
    -- Added Camera yoffset to give better view of scene. Will adjust value as needed based on level design.

--- Halloween Game/Assets/Scripts/CameraController.cs
+++ Halloween Game/Assets/Scripts/CameraController.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] Transform player;
+    Transform player;
 
+    [SerializeField] float yBuffer = 4f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         // position cannot simply be player position since the z-axis is different.
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y+yBuffer, transform.position.z);
     }
 }
