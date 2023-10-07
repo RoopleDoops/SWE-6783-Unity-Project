1deb6105043e7f84ad91e02da9f601881891cb3e
Jamakihe <143671844+Jamakihe@users.noreply.github.com>
2023-10-07 17:59:32 -0400

    Created Timer
    Created a 60 second Timer to complete level

---
+++ Halloween Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+
+    float currTme = 0f;
+    float startTme = 60f;
+    // Start is called before the first frame update
+
+    [SerializeField] Text Time_clock;
+
+    void Start()
+    {
+        currTme = startTme;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currTme -= 1 * Time.deltaTime;
+        Time_clock.text = currTme.ToString();
+
+        if (currTme <= 0)
+
+        {
+            currTme = 0;
+
+        }
+    }
+}
