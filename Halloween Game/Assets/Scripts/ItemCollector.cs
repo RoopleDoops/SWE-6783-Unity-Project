e9457cddffb2e482b1789feab3a2195cdaf7c68a
Jamakihe <143671844+Jamakihe@users.noreply.github.com>
2023-10-13 14:26:57 -0400

    health bar prefab
    health bar prefab

--- Halloween Game/Assets/Scripts/ItemCollector.cs
+++ Halloween Game/Assets/Scripts/ItemCollector.cs
@@ -6,10 +6,6 @@ using UnityEngine.UI;
 //put this script on the player
 public class ItemCollector : MonoBehaviour
 {
-    public GameObject Candy;
-    public GameObject Candy2;
-    public GameObject Candy3;
-    public GameObject Candy4;
     private int candies = 0;
 
     private PlayerMovement move;
@@ -38,25 +34,6 @@ public class ItemCollector : MonoBehaviour
             Destroy(collision.gameObject);
             candies++;
             candiesText.text = candies.ToString();
-            if (candies == 1)
-            {
-                Candy.SetActive(true);
-            }
-
-            else if(candies == 2)
-            {
-               Candy2.SetActive(true);
-            }
-
-            else if(candies == 3)
-            {
-                Candy3.SetActive(true);
-            }
-
-            else if(candies == 4)
-            {
-                Candy4.SetActive(true);
-            }
         }
     }
     public void win()
